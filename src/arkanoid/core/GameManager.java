868430dac48b263f565ea13c43352cb8d9ece67d
sadboizsitinh <buivinh2k66@gmail.com>
2025-10-29 12:31:44 +0700

    update: factory pattern

--- src/arkanoid/core/GameManager.java
+++ src/arkanoid/core/GameManager.java
@@ -2,10 +2,7 @@ package arkanoid.core;
 
 
 import arkanoid.entities.Ball.Ball;
-import arkanoid.entities.Brick.Brick;
-import arkanoid.entities.Brick.NormalBrick;
-import arkanoid.entities.Brick.StrongBrick;
-import arkanoid.entities.Brick.UnbreakableBrick;
+import arkanoid.entities.Brick.*;
 import arkanoid.entities.Paddle.Paddle;
 import arkanoid.entities.PowerUp.*;
 import javafx.scene.canvas.GraphicsContext;
@@ -282,9 +279,9 @@ public class GameManager {
 
                         double x = col * brickWidth, y = 50 + row * brickHeight;
                         switch (type) {
-                            case 1 -> bricks.add(new NormalBrick(x, y, brickWidth - 2, brickHeight - 2));
-                            case 2 -> bricks.add(new StrongBrick(x, y, brickWidth - 2, brickHeight - 2));
-                            case 3 -> bricks.add(new UnbreakableBrick(x, y, brickWidth - 2, brickHeight - 2));
+                            case 1 -> bricks.add(BrickFactory.createBrick(Brick.BrickType.NORMAL,x, y, brickWidth - 2, brickHeight - 2 ));
+                            case 2 -> bricks.add(BrickFactory.createBrick(Brick.BrickType.STRONG,x, y, brickWidth - 2, brickHeight - 2 ));
+                            case 3 -> bricks.add(BrickFactory.createBrick(Brick.BrickType.UNBREAKABLE,x, y, brickWidth - 2, brickHeight - 2 ));
                         }
                     }
                     row++;
